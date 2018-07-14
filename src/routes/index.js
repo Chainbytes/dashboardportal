0cc436e930c06a08eaa9e9e0f703f354d6bab455
Jameson Vallar <jvallar@gmail.com>
2018-07-14 10:04:37 +0800

    Added the dashboard links

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,37 @@
 import React from "react";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import {Menu, Container, Segment, Header} from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
-import Page1 from "./Page1";
-import Page2 from "./Page2";
-
+let dashboardData = [
+    {label: "Anguilla", links: "https://anguilla.atmconsole.com/#/login"},
+    {label: "ATMMM", links: "https://atmmm.atmconsole.com/#/login"},
+    {label: "BC Systems", links: "https://bcsystems.atmconsole.com/#/login"},
+    {label: "Bittrade", links: "https://bittrade.atmconsole.com/#/login"},
+    {label: "Chainbytes", links: "https://chainbytes.atmconsole.com/#/login"},
+    {label: "Cream Coin", links: "https://creamcoin.atmconsole.com/#/login"},
+    {label: "Hayden", links: "https://hayden.atmconsole.com/#/login"},
+    {label: "Japanese", links: "https://japanese.atmconsole.com/#/login"},
+    {label: "Netco Digital", links: "https://netcodigital.atmconsole.com/#/login"},
+    {label: "Rocket", links: "https://rocket.atmconsole.com/#/login"},
+    {label: "Silicon Prairie", links: "https://silicon-prairie.atmconsole.com/#/login"},
+    {label: "Texas Kevin", links: "https://texas-kevin.atmconsole.com/#/login"},
+    {label: "Zozo Coin", links: "https://zozocoin.atmconsole.com/#/login"},
+    {label: "Vizitiu", links: "https://vizitiu.atmconsole.com/#/login"},
+    {label: "Beeps", links: "https://beeps.atmconsole.com/#/login"},
+    {label: "Bayside", links: "https://bayside.atmconsole.com/#/login"}
+]
+let dashboardItem = dashboardData.map((data, index) => {
+    return <Menu.Item href={data.links} key={index}  target='_blank'>{data.label}</Menu.Item>
+})
 export default () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Page1} />
-      <Route exact path="/page2" component={Page2} />
-    </Switch>
-  </BrowserRouter>
+
+    <Container textAlign='center'>
+        <br/>
+        <Header as='h2' icon textAlign='center'>Dashboards</Header>
+        <Segment centered>
+            <Menu vertical>
+                {dashboardItem}
+            </Menu>
+        </Segment>
+    </Container>
 );
